dc8492a93e98b207acc73e460b3f2857b64eaa90
João Guilherme <dry_jg@hotmail.com>
2024-10-25 11:30:18 -0400

    Atualização Tela

--- js/scripts-joao/script.js
+++ js/scripts-joao/script.js
@@ -1,3 +1,13 @@
+// script.js
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
 document.addEventListener("DOMContentLoaded", function() {
     const currentYear = new Date().getFullYear();
     document.getElementById("currentYear").textContent = currentYear;
