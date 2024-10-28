451ab8d518719a5806910f923ec3359e2d26d8fc
Fred Lopes <fredlopes.priv@outlook.com>
2024-10-28 09:07:15 -0400

    update: animacao

--- js/fred-scripts/animacao.js
+++ js/fred-scripts/animacao.js
@@ -1,4 +1,15 @@
-document.addEventListener("DOMContentLoaded", function() {
-    const currentYear = new Date().getFullYear();
-    document.getElementById("current-year").textContent = currentYear;
-});
+const background = document.querySelector('.background-codes');
+
+for (let i = 0; i < 100; i++) {
+    const codeColumn = document.createElement('div');
+    codeColumn.classList.add('code-column');
+    codeColumn.style.left = `${Math.random() * 100}vw`;
+    codeColumn.style.animationDuration = `${Math.random() * 2 + 10}s`;
+    background.appendChild(codeColumn);
+
+    for (let j = 0; j < 20; j++) {
+        const codeChar = document.createElement('span');
+        codeChar.textContent = Math.floor(Math.random() * 2);
+        codeColumn.appendChild(codeChar);
+    }
+}
